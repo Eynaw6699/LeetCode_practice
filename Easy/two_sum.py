0f3660cc3f664d547210b5be94902b282f97e3df
Eynaw2369 <cweitsen@gmail.com>
2021-08-24 18:32:30 +0800

    add new exercise solution

---
+++ Easy/two_sum.py
@@ -0,0 +1,27 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+# target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+class Solution:
+    @staticmethod
+    def twoSum(nums, target):
+        values = {}
+        for i, num in enumerate(nums):
+            remaining = target - num
+            if remaining in values:
+                return [i, values[remaining]]
+            else:
+                values[num] = i
+
+
+if __name__ == "__main__":
+    a = Solution.twoSum(nums=[3, 2, 4], target=6)
+    print(a)
