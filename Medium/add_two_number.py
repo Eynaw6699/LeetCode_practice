af3b9811a131c6f1c987661077d8ab4661c9853a
Eynaw2369 <cweitsen@gmail.com>
2021-10-19 21:24:17 +0800

    add new medium exercise solution

---
+++ Medium/add_two_number.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def longestCommonPrefix(string_list: List[str]) -> str:
+        if not string_list:
+            return ""
+        m, M, i = min(string_list), max(string_list), 0
+        for i in range(0, len(M)):
+            if m[i] != M[i]:
+                break
+        else:
+            i += 1
+        return m[:i]
+
+
+if __name__ == "__main__":
+    S = ["flower", "flow", "flight"]
+    print(Solution.longestCommonPrefix(S))
+    S_1 = ["dog", "racecar", "car"]
+    print(Solution.longestCommonPrefix(S_1))
