e415801dced4e7d94ed77564e40668c47afe4b34
Eynaw2369 <cweitsen@gmail.com>
2021-10-19 21:30:09 +0800

    minor update

--- Medium/add_two_number.py
+++ Medium/add_two_number.py
@@ -23,5 +23,5 @@ class Solution:
 if __name__ == "__main__":
     S = ["flower", "flow", "flight"]
     print(Solution.longestCommonPrefix(S))
-    S_1 = ["dog", "racecar", "car"]
+    S_1 = ["dog", "race car", "car"]
     print(Solution.longestCommonPrefix(S_1))
