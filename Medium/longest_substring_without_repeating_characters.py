90150fc63f5ff5b75e318a50136ad835efc9a0fc
Eynaw2369 <cweitsen@gmail.com>
2021-11-21 18:24:34 +0800

    update Medium solution

---
+++ Medium/longest_substring_without_repeating_characters.py
@@ -0,0 +1,32 @@
+# Given a string s, find the length of the longest substring without repeating characters.
+#
+# Example 1:
+#
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+#
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+
+class Solution:
+    @staticmethod
+    def lengthOfLongestSubstring(s: str):
+        used = {}
+        max_length = start = 0
+        for i, c in enumerate(s):
+            if c in used and start <= used[c]:
+                start = used[c] + 1
+            else:
+                max_length = max(max_length, i - start + 1)
+
+            used[c] = i
+
+        return max_length
+
+
+if __name__ == "__main__":
+    a = Solution.lengthOfLongestSubstring(s="abbbbbbb")
+    print(a)
